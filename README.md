d38797d43c8f82b1220fbcc5b0318a500ffb1d4f
Brandon Seegraves <brandonseegraves@gmail.com>
2023-10-27 16:40:43 -0400

    Added text to README file and added .jar file

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Fibonacci
+Welcome to the fibonacci program. Just enter a number after running the 
+program to find the corresponding fibonacci sequence number or enter as
+an argument directly from the command line for the same effect.
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int input = 0;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello! Welcome to the Fibonacci program.");
+        System.out.println("Please enter the sequence number of your desired fibonacci number if not already entered.");
+
+
+        if (args.length > 0) {
+            try {
+                input = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                System.err.println("Input must be an integer, please try again");
+                System.exit(1);
+            }
+        }
+        else {
+            input = scanner.nextInt();
+        }
+        
+        System.out.println("The " + input + " number of the fibonacci sequence is " + fibonacciFinder(input));
+
+    }
+    public static  int fibonacciFinder(int sequenceNum) {
+        int f0 = 0;
+        int f1 = 1;
+        int fn;
+
+        if (sequenceNum == 0) {
+            return f0;
+        }
+
+        for (int i = 2; i <= sequenceNum; i++) {
+            fn = f0 + f1;
+            f0 = f1;
+            f1 = fn;
+
+        }
+        return f1;
     }
 }
